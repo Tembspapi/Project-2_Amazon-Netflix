7d8e44f6b627f4d10e01b8633514fddda71c4901
gwsc <111397774+gw-sc@users.noreply.github.com>
2022-11-23 19:47:40 +0000

    upd

--- Project2.sql
+++ Project2.sql
@@ -61,6 +61,40 @@ where amazon.director like 'C%'
 and amazon.category like '%Drama%'
 or netflix.category like '%Drama%'; 
 
--- 1. compare number of titles released each year
-select count(distinct title) as titles
-from netflix;
+
+-- 1. Netflix - compare number of titles released each year
+select release_year, count(distinct title) as titles
+from netflix
+GROUP by release_year;
+
+--2. Netflix - compare number of released categories each year
+select release_year, count(distinct category) as category
+from netflix
+GROUP by release_year;
+
+--3. Netflix - most active director
+select director, count(distinct title) as titles
+from netflix
+where director is not null
+GROUP by director
+order by titles DESC
+fetch first 5 rows only;
+
+--4. Netflix - which country releases the most each year?
+select country, release_year, count(distinct title) as titles
+from netflix
+GROUP by country, release_year
+order by titles DESC
+fetch first 5 rows only;
+
+--5. Movies title that are in both Netflix and Amazon
+select distinct netflix.title, amazon.title
+from netflix inner join amazon
+on netflix.title = amazon.title
+
+--6. Movies that are exclusively to netflix
+select distinct netflix.title
+from netflix
+where netflix.title not in (select distinct amazon.title
+from netflix inner join amazon
+on netflix.title = amazon.title)
